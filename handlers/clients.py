eb90b057cb7cba8525435f0f3c3a2a0f2e5c9116
muiouls <Jaka.Momunalieva07@mail.ru>
2023-04-14 16:34:29 +0600

    hw2

--- handlers/clients.py
+++ handlers/clients.py
@@ -38,7 +38,20 @@ async def mem(message: types.Message):
     await message.answer_photo(photo=open('/Users/mac/Downloads/361a557a4442607d0bb5-1024x576.jpg', 'rb'))
 
 
+async def dice_game(message: types.Message):
+    dice_bot = await bot.send_dice(message.chat.id)
+    dice_user = await bot.send_dice(message.chat.id)
+    await message.answer("Сначала кинул кость бот, затем пользователь")
+    if dice_bot.dice.value > dice_user.dice.value:
+        await message.answer("Бот победил")
+    elif dice_bot.dice.value == dice_user.dice.value:
+        await message.answer("Ничья")
+    else:
+        await message.answer("Пользователь победил")
+
+
 def register_handlers_clients(dp: Dispatcher):
     dp.register_message_handler(start_command, commands=['start'])
     dp.register_message_handler(quiz_1, commands=['quiz'])
     dp.register_message_handler(mem, commands=['mem'])
+    dp.register_message_handler(dice_game, commands=['dice'])
